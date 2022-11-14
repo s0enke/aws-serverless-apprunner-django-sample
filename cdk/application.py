cf0732e5c09f48bb589fdfb2bb418264338179e8
Soenke Ruempler <soenke@ruempler.eu>
2022-11-14 16:37:26 +0100

    chore: remove hardcoded arn

--- cdk/application.py
+++ cdk/application.py
@@ -1,5 +1,6 @@
 from aws_cdk import (
     Stack,
+    CfnParameter
 )
 import aws_cdk.aws_apprunner_alpha as apprunner
 
@@ -10,13 +11,18 @@ class InfrastructureStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        apprunner_source_connection_arn_parameter = CfnParameter(
+            self,
+            "SourceConnectionArn"
+        )
         service = apprunner.Service(self, "whats_my_weather",
                                     source=apprunner.Source.from_git_hub(
                                         repository_url="https://github.com/s0enke/aws-serverless-apprunner-django-sample",
                                         branch="master",
                                         configuration_source=apprunner.ConfigurationSourceType.REPOSITORY,
                                         connection=apprunner.GitHubConnection.from_connection_arn(
-                                            "arn:aws:apprunner:eu-west-1:255296336815:connection/apprunner/59f079c743b94d5c98f5a619052d4ffb")
+                                            apprunner_source_connection_arn_parameter.value_as_string
+                                        )
                                     )
                                     )
         cfn_service = service.node.default_child
