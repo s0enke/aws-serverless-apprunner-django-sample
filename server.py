57e337c82cb297be3af6d1721f977a73f3c79349
Soenke Ruempler <soenke@ruempler.eu>
2022-11-14 13:44:23 +0100

    even simpler

--- server.py
+++ server.py
@@ -11,10 +11,9 @@ def hello_world(request):
     return Response(message)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT"))
     with Configurator() as config:
         config.add_route('hello', '/')
         config.add_view(hello_world, route_name='hello')
         app = config.make_wsgi_app()
-    server = make_server('0.0.0.0', port, app)
+    server = make_server('0.0.0.0', 8080, app)
     server.serve_forever()
